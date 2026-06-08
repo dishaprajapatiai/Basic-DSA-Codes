79dabfb01f0e7c469b71b0f8c7022fde9b0f4988
disha9213 <dishap9213@gmail.com>
2026-06-08 23:07:42 +0530

    Added a code of reverse a string

---
+++ reverseString.java
@@ -0,0 +1,10 @@
+class reverseString{
+    public static void main(String[] args) {
+        String s = "Hello World";
+        String rev = "";
+        for(int i =s.length()-1; i>=0 ;i--){
+            rev += s.charAt(i);
+        }
+        System.err.println(rev);
+    }
+}
