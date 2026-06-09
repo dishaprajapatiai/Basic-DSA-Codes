26d220036436fd962264aedf805507e34294debe
disha9213 <dishap9213@gmail.com>
2026-06-09 10:02:40 +0530

    Added a anagram code and another way of palindrome as well

---
+++ anagram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class anagram {
+    public static void main(String[] args) {
+        String s = "hello";
+        String t = "elolh";
+
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        if(Arrays.equals(c1,c2)){
+            System.out.println("They are anagram");
+        }else{
+            System.out.println("not anagram");
+        }
+    }
+}
--- palindrome.java
+++ palindrome.java
@@ -1,24 +1,45 @@
-// package Basic-DSA-Codes;
+// // package Basic-DSA-Codes;
 
-public class palindrome {
-    public static void main(String[] args) {
-        String s = "madsam";
-        boolean isPalindrome = true;
+// public class palindrome {
+//     public static void main(String[] args) {
+//         String s = "madsam";
+//         boolean isPalindrome = true;
 
-        int mid = s.length()/2;
-        for(int i = 0; i<mid ;i++){
-        int n = s.length();
+//         int mid = s.length()/2;
+//         for(int i = 0; i<mid ;i++){
+//         int n = s.length();
 
-        if(s.charAt(i)!= s.charAt(n-1-i)){
-            isPalindrome = false;
-            break;
-        }
-     }
-     if(isPalindrome){
-        System.out.println("It is a Palindrome");
-     }else{
-        System.out.println("Not a palindrome");
-     }
-    }
+//         if(s.charAt(i)!= s.charAt(n-1-i)){
+//             isPalindrome = false;
+//             break;
+//         }
+//      }
+//      if(isPalindrome){
+//         System.out.println("It is a Palindrome");
+//      }else{
+//         System.out.println("Not a palindrome");
+//      }
+//     }
     
+// }
+
+
+
+
+
+// 2nd way
+public class palindrome{
+    public static void main(String[] args) {
+        String s = "madam";
+        String rev = "";
+        for(int i = s.length()-1;i>=0;i--){
+            rev += s.charAt(i);
+        }
+
+        if(rev.equals(s)){
+            System.out.println("It is Palindrome");
+        }else{
+            System.out.println("It is not Palindrome");
+        }
+    }
 }
