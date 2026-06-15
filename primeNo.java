d78d1519fc8b4defcfb268a40fbaf7b5e5646a11
disha9213 <dishap9213@gmail.com>
2026-06-15 07:06:18 +0530

    Added code for primeNo

---
+++ primeNo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class primeNo{
+    public static void main(String[] args){
+        int n = 37;
+        boolean isPrimeNum = true;
+        if(n <= 1){
+            System.out.println("Not a prime no");
+        }
+        for(int i = 2 ;i< Math.sqrt(n) ;i++){
+            if(n % i == 0){
+                isPrimeNum = false;
+                break;
+            }
+
+        }
+        if(isPrimeNum){
+            System.out.println("It is a prime no");
+        }else{
+             System.out.println("It is not a prime no");
+        }
+    }
+
+}
