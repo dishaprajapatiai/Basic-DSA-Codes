132a0d04430afffad664da4806b5b82e9d1e8d54
disha9213 <dishap9213@gmail.com>
2026-06-16 22:24:07 +0530

    Added code for reverse words in a string

---
+++ reverseWords.java
@@ -0,0 +1,17 @@
+public class reverseWords {
+    public static void main(String[] args) {
+        String s = "Hello my name is Disha";
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = words.length-1;i>=0 ;i--){
+            sb.append(words[i]);
+
+            if( i != 0){
+            sb.append(" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+}
