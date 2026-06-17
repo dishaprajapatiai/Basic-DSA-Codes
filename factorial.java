76622c8e051b256d03ca300e52e3825f37f08896
disha9213 <dishap9213@gmail.com>
2026-06-17 07:41:18 +0530

    Factorial using recursion

--- factorial.java
+++ factorial.java
@@ -1,11 +1,29 @@
+// public class factorial {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         long fact = 1;
+//         for(int i = 1 ;i<=n ;i++){
+//             fact = fact*i;
+//         }
+//         System.out.println("Factorial of " + n+ " is " +fact);
+//     }
+    
+// }
+
+
+
+// with recursion
 public class factorial {
     public static void main(String[] args) {
         int n = 5;
-        long fact = 1;
-        for(int i = 1 ;i<=n ;i++){
-            fact = fact*i;
+        System.out.println(fact(n));
+        
+    }
+    public static int fact(int n){
+        if(n == 1 || n == 0){
+            return 1;
         }
-        System.out.println("Factorial of " + n+ " is " +fact);
+            return fact(n-1) * n;
     }
     
 }
