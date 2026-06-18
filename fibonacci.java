8b96cf5c84f4231dfdc3a3e67570301c90c536e3
disha9213 <dishap9213@gmail.com>
2026-06-18 18:28:44 +0530

    Added code with recursion

--- fibonacci.java
+++ fibonacci.java
@@ -1,15 +1,38 @@
-public class fibonacci {
-    public static void main(String[] args) {
-        int n = 5;
-        int a = 0;
-        int b = 1;
+// // Without Recursion
+// public class fibonacci {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         int a = 0;
+//         int b = 1;
 
-        System.out.println("Fibonacci");
+//         System.out.println("Fibonacci");
+//         for (int i = 0; i < n; i++) {
+//             System.out.println(a+ " ");
+//             int c = a+b;
+//             a = b;
+//             b = c;
+//         }
+//     }
+// }
+
+
+
+// WITH RECURSION
+public class fibonacci{
+    public static void main(String[] args){
+        int n = 5;
         for (int i = 0; i < n; i++) {
-            System.out.println(a+ " ");
-            int c = a+b;
-            a = b;
-            b = c;
+             System.out.println(fib(i)+ " ");
         }
+       
+    }
+
+    public static int fib(int n){
+        
+        if(n <= 1){
+            return n;
+        }
+
+        return fib(n-1)+fib(n-2);
     }
 }
