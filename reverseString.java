1bbce013449ebb8b2c7b0321a4aef6612bac03a6
disha9213 <dishap9213@gmail.com>
2026-07-09 14:54:22 +0530

    A new code in reverse string

--- reverseString.java
+++ reverseString.java
@@ -1,10 +1,28 @@
+// class reverseString{
+//     public static void main(String[] args) {
+//         String s = "Hello World";
+//         String rev = "";
+//         for(int i =s.length()-1; i>=0 ;i--){
+//             rev += s.charAt(i);
+//         }
+//         System.err.println(rev);
+//     }
+// }
+
+import java.util.Arrays;
 class reverseString{
     public static void main(String[] args) {
-        String s = "Hello World";
+        char[] arr = {'h' ,'e' ,'l','l','o'};
+        String str = new String(arr);
         String rev = "";
-        for(int i =s.length()-1; i>=0 ;i--){
-            rev += s.charAt(i);
+
+        for(int i = str.length() -1; i >= 0 ;i--){
+          rev = rev + str.charAt(i);
         }
-        System.err.println(rev);
+
+        for(int j = 0 ; j< rev.length();j++){
+            arr[j] = rev.charAt(j);
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
